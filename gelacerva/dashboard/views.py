196b0cced139598f3923fac9526b747206e046c1
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-06 11:16:38 -0300

    Fix template

--- gelacerva/dashboard/views.py
+++ gelacerva/dashboard/views.py
@@ -14,5 +14,7 @@ def index(request):
     today_plus_one = datetime.now().date() + timedelta(days=1)
     context = {}
     context["temps"] = Temperatures.objects.filter(date__gte=a_week_ago, date__lt=today_plus_one)
+    context["temps"].group_by('device')
+    # context["temps"].annotate(mean_temperature=Avg('temperature'))
 
     return render(request, template_name="dashboard/index.html", context=context)

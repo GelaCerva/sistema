109ff34d5f2a02b7188d05d0ad3fb696feea474d
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2018-08-11 19:36:26 -0300

    Settings changed

--- gelacerva/gelacerva/settings.py
+++ gelacerva/gelacerva/settings.py
@@ -129,9 +129,11 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 LOGIN_REDIRECT_URL = 'home'
